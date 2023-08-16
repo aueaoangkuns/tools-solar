bb07648030da64decf59497386db3c991816d4f6
aueaoangkuns <aueaoangkun_s@npp.co.th>
2023-08-16 10:18:13 +0700

    Test
    ss

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+// Connect Database
+let knex = require('knex')({
+    client: 'mssql',
+    connection: {
+        server : 'DESKTOP-9PESU7N',
+        user : 'sroiaudom',
+        password : '0822914530aA',
+        database : 'Test'
+    }
+});
+
+// config ค่าเพื่อเข้า HTTP API Huawei
 const axios = require('axios');
 require('dotenv').config()
 const baseUrl = 'https://intl.fusionsolar.huawei.com/thirdData/'
+
 // ส่วนของ Login
 async function hwLogin() {
     const data = {
@@ -39,10 +52,10 @@ async function getDeviceData(DeviceCode,DevIdType){
 }
 // การแสดงผลข้อมูล
 async function writeDB() {
-    const response = await getDeviceData('1000000033980716,1000000033980715,1000000033980714,1000000033980713,1000000033980712,1000000033980711,1000000033980710,1000000033980709,1000000033980724,1000000033980723,1000000033980722,1000000033980721,1000000033980720,1000000033980719,1000000033980718,1000000033980717,1000000033980728,1000000033980727,1000000033980726,1000000033980725,1000000033980862,1000000033980861,1000000033980860,1000000033980859,1000000033980858,1000000033980857,1000000033980856,1000000033980855,1000000033980870,1000000033980869,1000000033980868,1000000033980867,1000000033980866,1000000033980865,1000000033980864,1000000033980863,1000000033980871,1000000033980854,1000000033980853,1000000033980682,1000000033980681,1000000033980680,1000000033980679,1000000033980678,1000000033980677,1000000033980676,1000000033980675,1000000033980688,1000000033980687,1000000033980686,1000000033980685,1000000033980684,1000000033980683,1000000033980674,1000000033980673,1000000033980672,1000000033980671,1000000033980670,1000000033980669','1')
+    const response = await getDeviceData('1000000033980716,1000000033980715,1000000033980714,1000000033980713,1000000033980712,1000000033980711,1000000033980710,1000000033980709,1000000033980724,1000000033980723,1000000033980722','1')
     const obj = response
 
-    for (i = 0 ; i <= 58 ; i++ )
+    for (i = 0 ; i <= 10 ; i++ )
     {
         const obj2 = response.data[i]
         const time = obj.params['currentTime']
@@ -51,9 +64,23 @@ async function writeDB() {
         const devID = obj2.devId
         const ActivePower = dataItemMap['active_power']
 
-        const sql = `(${time},${devID},${ActivePower})`
+        const sql = `PH3 (${devID},${ActivePower},${time})`
         console.log(sql)
-    }
-}
 
+        const insertDemo = (DEVID, ACTPOWER, TIME ) => {
+            knex('Test_API').insert({
+                 devId: DEVID,
+                 active_power: ACTPOWER,
+                 currentTime: TIME
+            }).then((countRows)=>{
+                console.log(`${countRows} number of records have been insert`)
+            }).finally(function(){
+                knex.destroy();
+            });
+        };
+        insertDemo(devID,ActivePower,time)
+        
+    }
+    
+}
 writeDB();
