bc05dd4ef109e7572bd49e7de9c82cc9c2173f8d
aueaoangkuns <aueaoangkun_s@npp.co.th>
2023-08-17 10:17:02 +0700

    d
    1

--- Test_Time.js
+++ Test_Time.js
@@ -51,23 +51,21 @@ async function getDeviceData(DeviceCode,DevIdType){
     }
 }
 
-
-
-
 const cron = require("node-cron")
 function create_datetime(seconds, minute, hour, day, month, day_of_week){
     return seconds + " " + minute + " " + hour + " " + day + " " + month + " " + day_of_week
 }console.log("Start!!")
 
-cron.schedule(
-    create_datetime('*/40', '*', '*', '*', '*', '*'),
+cron.schedule('* */30 * * * *', () => {
+    writeDB();
+})
 
-        // การแสดงผลข้อมูล
-    async function writeDB() {
-        const response = await getDeviceData('1000000033980716,1000000033980715,1000000033980714,1000000033980713,1000000033980712,1000000033980711,1000000033980710,1000000033980709,1000000033980724,1000000033980723,1000000033980722','1')
-        for (i = 0 ; i <= 10 ; i++ )
-        {
-            const obj = response
+async function writeDB() {
+    try {
+        const response = await getDeviceData('1000000033980716,1000000033980715,1000000033980714,1000000033980713,1000000033980712,1000000033980711,1000000033980710,1000000033980709,1000000033980724,1000000033980723,1000000033980722,1000000033980721,1000000033980720,1000000033980719,1000000033980718,1000000033980717,1000000033980728,1000000033980727,1000000033980726,1000000033980725,1000000033980862,1000000033980861,1000000033980860,1000000033980859,1000000033980858,1000000033980857,1000000033980856,1000000033980855,1000000033980870,1000000033980869,1000000033980868,1000000033980867,1000000033980866,1000000033980865,1000000033980864,1000000033980863,1000000033980871,1000000033980854,1000000033980853,1000000033980682,1000000033980681,1000000033980680,1000000033980679,1000000033980678,1000000033980677,1000000033980676,1000000033980675,1000000033980688,1000000033980687,1000000033980686,1000000033980685,1000000033980684,1000000033980683,1000000033980674,1000000033980673,1000000033980672,1000000033980671,1000000033980670,1000000033980669', '1')
+        const obj = response
+
+        for (i = 0; i <= 58; i++) {
             const obj2 = response.data[i]
             const time = obj.params['currentTime']
 
@@ -75,22 +73,21 @@ cron.schedule(
             const devID = obj2.devId
             const ActivePower = dataItemMap['active_power']
 
-            const sql = `PH4 No.${i+1} (${devID},${ActivePower},${time})`
-            console.log(sql)
-            const insertDemo = (DEVID, ACTPOWER, TIME ) => {
-                knex('Test_API').insert({
-                     devId: DEVID,
-                     active_power: ACTPOWER,
-                     currentTime: TIME
 
-                }).finally(function(){
-                    knex.destroy();
-                });
-            };
-            insertDemo(devID,ActivePower,time)
-                        
+            const sql = `PH4 No.${i + 1} (${devID},${ActivePower},${time})`
+            console.log(sql)
+            await knex('Test_API').insert({
+                devId: devID,
+                active_power: ActivePower,
+                currentTime: time
+            })
         }
         console.log(`-------------   Wait 2 min     --------------------`)
-        writeDB(end)
+
     }
-)
+    catch (e) {
+        console.log('Error', e.message)
+    }
+
+
+}
